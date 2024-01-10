f7412c90c9b5c32c3c8959dd6e994dbaf64f3e8b
Tiago <tiago8204@gmail.com>
2024-01-09 21:24:39 -0300

    Update ESPNOW Print Function

--- src/EspNowSerial.h
+++ src/EspNowSerial.h
@@ -4,15 +4,18 @@
 
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Print.h>
 
 struct DataStruct {
   char message[100];
 };
 
-class EspNowSerial {
+class EspNowSerial : public Print { // A classe EspNowSerial herda da classe Print
   private:
     uint8_t *targetMAC;
     esp_now_peer_info_t peerInfo;
+
+    //Print espNowSerial;
 
     static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
       // Função chamada quando os dados são enviados
@@ -29,6 +32,18 @@ class EspNowSerial {
     EspNowSerial(uint8_t *macAddress) {
       targetMAC = macAddress;
     }
+
+    size_t write(uint8_t c) override { // Sobrescreve a função write da classe Print
+        // Esta implementação ainda é necessária para outros usos de write
+        esp_now_send(targetMAC, &c, sizeof(uint8_t));
+        return 1; // Retorna o número de bytes escritos
+    }
+
+    size_t write(const uint8_t *buffer, size_t size) override {
+        esp_now_send(targetMAC, buffer, size);
+        return size; // Retorna o número de bytes escritos
+    }
+
 
     void initESPNow() {
       WiFi.disconnect();
@@ -59,7 +74,21 @@ class EspNowSerial {
 
     void disableESPNow() {
       esp_now_deinit(); // Desativa o ESP-NOW
+      
     }
 };
+
+// class espNowSerialClass : public Print {
+// public:
+//   size_t write(uint8_t c) {
+//     // Convert the uint8_t value to a String
+//     String message = String(c);
+//     // Implemente aqui a lógica para enviar 'c' usando ESP-NOW
+//     espNowSerial.sendData(message);
+//     return 1; // Retorna o número de bytes escritos
+//   }
+// };
+
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include "MultiEncoder.h"
+#include "PID/dualpid.h"
 #include "Pins.h"
 #include "rfid_functions.h"
 #include "EspNowSerial.h"
 #include "Led.h"
 #include "webserialmenu.h"
 #include "MotorDriver.h"
-bool verdade = true;
 
-#include "PID/dualpid.h"
+
+bool move = false;
 
 uint8_t macAddress2Esp32[] = { 0xA8, 0x42, 0xE3, 0xCB, 0x82, 0xEC }; // Replace with the target ESP32's MAC
 EspNowSerial espNowSerial(macAddress2Esp32);
@@ -17,11 +18,9 @@ dualPID dualpid;
 MFRC522 mfrc522(SS_RFID_PIN, RST_RFID_PIN);
 MotorDriver motor(EN1, IN2, IN1, EN2, IN3, IN4);
 
-int a = 0;
-bool move = false;
 
 void myFunction111(){
-    a = 1;
+
 }
 void myFunction3() {
     //motor.moveForward(move ? 0 : 300);  // Operador ternário (? :) Se move for verdadeiro, escolhemos 0 (parar o motor). Se move for falso, escolhemos 300.
@@ -32,32 +31,27 @@ void myFunction3() {
 }
 
 void myFunction2(){
-    analogWrite(EN2, 0);
-    a = 0;
+
 }
 
 void printEspNow(){
-    //dualpid.setInput1(getpulse1());
-    //dualpid.setInput2(getpulse2());
+
     dualpid.updatePID(getpulse1(), getpulse2());
     motor.setSpeed(dualpid.getOutput1(), dualpid.getOutput2());
 
-   
+    //espNowSerial.sendData(String(getpulse1())+","+String(getpulse2())); // Plot
+    
+    
 
-    espNowSerial.sendData(String(getpulse1())+","+String(getpulse2())); // Plot
-    
-    //espNowSerial.sendData(String(dualpid.getOutput1())+","+String(dualpid.getInput1())); // Convert the integer to a string before sending
-    
-    //espNowSerial.sendData("motor2: ");
-    
+    espNowSerial.print(dualpid.getOutput1()+","+dualpid.getInput1()); // Convert the integer to a string before sending
 }
 
 void setup() {
-    //Serial.begin(115200);
 
-    //espNowSerial.initESPNow(); // Initialize ESP-NOW communication
+    espNowSerial.initESPNow();
 
     analogWriteResolution(10);
+    startEncoder(M1_S1, M2_S1);
 
     SPI.begin(); // Inicializa a biblioteca SPI para comunicação com o módulo RFID
     mfrc522.PCD_Init(); // Inicializa o módulo RFID
@@ -66,40 +60,12 @@ void setup() {
     RFID::addCardFunction( 0x104a4913, myFunction111);
     RFID::addCardFunction( 0x10602403, myFunction3);
 
-    startEncoder(M1_S1, M2_S1);
-
-    
-    //setupPIDBT();
-
-    
-    //setupwebserial();
-    espNowSerial.initESPNow();
-    
-
-
-
 }
-
 
 void loop() {
 
-    
-
-    //loopTestautoPID();
     RFID::checkRFIDPresent(mfrc522);
-
-    // if(a == 1){
-    //     loopPID();
-    // }
-
-    if(move){
-
-    updateEncoder(printEspNow);
-
-    }
-    
-
-    
+    if(move){updateEncoder(printEspNow);}
 
 }
 
