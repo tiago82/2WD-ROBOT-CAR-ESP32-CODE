c45f11ef41a5d611ffc9348935bc6ee08ba65cc1
Tiago <tiago8204@gmail.com>
2024-01-21 18:21:41 -0300

    Update - Remove old files

--- src/WIFI/WiFiConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_CONFIG_H
-#define WIFI_CONFIG_H
-
-#include <WiFi.h>
-
-void WifiInit(const char* ssid, const char* password);
-void WifiInit(const char* ssid, const char* password, IPAddress ip, IPAddress gateway, IPAddress subnet);
-
-#endif
